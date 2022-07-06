d68326783f791632414d80d6079ae24512f77dbf
ChiobiJason <chiobichisom@gmail.com>
2022-07-06 07:15:06 +0000

    Finished Challenge

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import notes from "../notes";
 import Note from "./Note";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(note => (
+        <Note
+        key={note.key}
+        title={note.title}
+        content={note.content}
+        />
+      )
+      )}
       <Footer />
     </div>
   );
